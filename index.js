220f17404d649bc520f12126d452df46e734378d
kubra islami <kubra.islami.00@gmail.com>
2025-05-26 05:48:48 +0100

    worked on using corsOptions and accessing user to fetch data

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 import express from 'express';
 import morgan from 'morgan';
-// import cors from 'cors';
+import cors from 'cors';
 import routes from "./src/routes/routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // or '*'
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// CORS options (optional: restrict to frontend origin)
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://afghan-proverbs-api-kysw.onrender.com',"*"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-    }
+app.use(cors({ origin: '*' }));
 
-    next();
-});
+// Use CORS middleware
+app.use(cors(corsOptions));
+
+// Middleware for preflight requests
+app.options('*', cors(corsOptions));
+
+
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(204);
+//     }
+//
+//     next();
+// });
 
 
 // Logging and body parsing
