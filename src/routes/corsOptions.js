f9409eb2b681d42cebcd04c573b03401acc7ef6c
kubra islami <kubra.islami.00@gmail.com>
2025-07-21 08:55:45 +0100

    worked on cors

--- src/routes/corsOptions.js
+++ src/routes/corsOptions.js
@@ -1,8 +1,20 @@
+//
+// const corsOptions = {
+//     origin: ['http://localhost:5173', 'https://afghan-proverbs-api-kysw.onrender.com'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     // allowedHeaders: ['Content-Type', 'Authorization'],
+//     allowedHeaders: ['Content-Type'],
+// };
+// export default corsOptions;
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://afghan-proverbs-api-kysw.onrender.com'],
+    origin: [
+        'http://localhost:5173',  // dev frontend
+        'https://afghan-proverbs-front-1.onrender.com'  // deployed frontend
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // optional if you use auth tokens
 };
+
 export default corsOptions;
+
